f039d790f9c5f243bbb3dba4a19c0db37f9dd770
eu_naaa <95084229+euniece@users.noreply.github.com>
2022-06-30 14:40:00 +0800

    Add files via upload

---
+++ report.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class report extends StatefulWidget{
+
+  @override
+  _report createState() => _report();
+}
+class _report extends State<report> {
+  late TextEditingController controller;
+
+  String bugReport = ' ';
+
+  @override
+  void initState() {
+   super.initState();
+
+   controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+   controller.dispose();
+
+   super.dispose();
+  }
+
+@override
+  Widget build(BuildContext context) => Scaffold(
+      
+        appBar: AppBar(
+         
+          title: const Text(
+            "SETTINGS",
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.black,
+              fontFamily: 'Poppins-ExtraBold',
+              fontWeight: FontWeight.w800, 
+            ),
+          ),
+          actions: <Widget>[
+            Image.asset("images/logo.png", width: 50,),
+            SizedBox(width: 15),
+          ],
+          
+           backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Container(
+            padding: const EdgeInsets.all(8.0),
+            margin:const EdgeInsets.all(8.0) ,
+            child: ListView(
+             children: <Widget>[
+               Text('ACCOUNT',
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.black,
+              fontFamily: 'Poppins-Medium',
+              fontWeight: FontWeight.w500, 
+            ),
+           ),
+            Card(
+           shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.circular(4), 
+                   side: BorderSide(color: Color.fromRGBO(5, 195, 107, 50),
+                   width: 0),),
+          elevation: 2,
+          child: ListTile(
+            leading: Icon(
+                        Icons.account_circle, color: Colors.black,
+                      ),
+            title: Text('Name'),
+            trailing: Icon(Icons.arrow_forward_ios_outlined),
+            onTap: () async{
+             final report = await openDialog(context);
+             if (report == null || report.isEmpty) return;
+             setState(() => this.bugReport = report);
+              },
+          ),
+        ),
+             ]),
+
+        ),
+
+  );
+
+Future<String?> openDialog(BuildContext context) => showDialog<String> (
+    context: context,
+    builder: (context) => AlertDialog (
+      
+        title: Text('What Happened?'),
+        content: TextField(
+          maxLines: 18,
+          autofocus: true,
+          decoration: InputDecoration(hintText: 'Briefly explain what happened and what we need to do to reproduce the problem.',
+          border: OutlineInputBorder(),
+          ), 
+          controller: controller,
+          onSubmitted: (_) => submit(context),
+        ),
+        actions: [
+          Container(
+                padding: EdgeInsets.all(12),
+                 alignment: FractionalOffset.topRight,
+                  child: GestureDetector(     
+                   child: FloatingActionButton.extended(
+                    label: Text('Send'), // <-- Text
+                    backgroundColor: Color.fromRGBO(5, 195, 107, 50),
+                    icon: Icon( // <-- Icon
+                      Icons.download,
+                      size: 18.0,
+                    ),
+                    onPressed: () {},
+        ),
+       ),
+      ),
+    ],
+  ),
+);
+
+void submit(BuildContext context) {
+  Navigator.of(context).pop(controller.text);
+  controller.clear();
+}
+
+
+}
